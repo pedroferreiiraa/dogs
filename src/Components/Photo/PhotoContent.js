89a65966d933ceb2d1092fc2ce0435de0fa90ad6
Pedro <pdro5k@outlook.com>
2022-09-10 21:59:15 -0300

    Adicionado image skeleton para imagem sendo carregada pelo fetch

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../../UserContext';
-import PhotoComments from './PhotoComments';
 import styles from './PhotoContent.module.css';
+import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
 import PhotoDelete from './PhotoDelete';
+import Image from '../Helper/Image';
 
 const PhotoContent = ({ data }) => {
   const user = React.useContext(UserContext);
@@ -12,7 +13,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
@@ -22,7 +23,6 @@ const PhotoContent = ({ data }) => {
             ) : (
               <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             )}
-
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className="title">
